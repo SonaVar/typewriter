980601d5043f17689c1b96c6917034ad28f2219a
Sona Varghese <sonasusan.v@gmail.com>
2020-11-03 00:30:11 +0000

    Commit changes to typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,5 @@
 const sentence = "hello there from lighthouse labs";
-let delay = 500;
+let delay = 0;
 for (let i = 0; i < sentence.length; i ++) {
   setTimeout(() => {
     process.stdout.write(sentence[i]);
